0e05f6405f361cd02195a07f42bd2736578d60c4
MrTibet <vader.ctylxy@gmail.com>
2021-02-17 21:48:48 +0200

    2.17.21

--- books.js
+++ books.js
@@ -1,1 +1,16 @@
-const booklist = {};
+const booklist = [
+    {
+        title: 'Saint X: A Novel',
+        author: 'Alexis Schaitkin',
+        url: 'https://m.media-amazon.com/images/I/51pMrW0+vcL.jpg',
+        plot: 'Saint X is hypnotic. Schaitkins characters...are so intelligent and distinctive it feels not just easy, but necessary, to follow them. I devoured [it] in a day'
+    },
+    {
+        title: 'The Four Winds',
+        author: 'Kristin Hannah',
+        url: 'https://images-na.ssl-images-amazon.com/images/I/512D7InYi7L._SX327_BO1,204,203,200_.jpg',
+        plot: 'The Four Winds seems eerily prescient in 2021 . . . Its message is galvanizing and hopeful: We are a nation of scrappy survivors. We’ve been in dire straits before; we will be again. Hold your people close'
+    }
+];
+
+localStorage.setItem('books',JSON.stringify(booklist));
--- index.js
+++ index.js
@@ -1,12 +1,33 @@
+let someArr =JSON.parse(localStorage.getItem('booklist'));
+let bookList = someArr.slice();
 let title = document.getElementById('bookTitle'),
     author = document.getElementById('authorName'),
     url = document.getElementById('bookImage'),
     plot = document.getElementById('descrip')
     submit = document.querySelector('.sub-btn');
 
-submit.addEventListener('click', function() {
-    localStorage.setItem('title', title.value)
-    localStorage.setItem('author', author.value)
-    localStorage.setItem('url', url.value)
-    localStorage.setItem('plot', plot.value)    
-})
+
+let postArr = [];
+
+function bookData() {
+    let postObjs = {};
+
+    postObjs['title'] = title.value;
+    postObjs['author'] = author.value;
+    postObjs['url'] = url.value;
+    postObjs['plot'] = plot.value;
+
+    /*localStorage.setItem('',JSON.stringify(postObjs));
+    postArr.push(JSON.parse(localStorage.getItem('')));*/
+
+    postArr.push(postObjs);
+
+    console.log(postArr);
+
+    bookList.push(postArr[0]);
+    console.log(bookList);
+
+}
+
+submit.addEventListener('click', bookData());
+
